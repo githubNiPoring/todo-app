2b5b5c700905448e29fff92fcfc993c884c26868
githubNiPoring <githubCV@gmail.com>
2024-11-14 22:15:19 +0800

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,95 @@
+import { useEffect, useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap-icons/font/bootstrap-icons.css";
+
+import TodoItem from "./TodoItem";
+
+//Add API base
+const API_BASE = "http://localhost:5000/todo";
+
+function App() {
+  const [items, setItems] = useState([]);
+  // Add input state, we will store the user's input in this state
+  const [input, setInput] = useState("");
+
+  useEffect(() => {
+    GetTodos();
+  }, []);
+
+  // Store the target's value into the input state
+  const handleChange = (e) => {
+    setInput(e.target.value);
+  };
+
+  const GetTodos = async () => {
+    fetch(API_BASE)
+      .then((res) => res.json())
+      .then((data) => setItems(data))
+      .catch((err) => console.log(err));
+  };
+  const addItem = async () => {
+    if (!input.trim()) {
+      alert("Please enter a to-do item.");
+      return;
+    }
+    const data = await fetch(API_BASE + "/new", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify({
+        name: input,
+      }),
+    }).then((res) => res.json());
+    await GetTodos();
+    setInput("");
+  };
+
+  const handleKeyDown = (e) => {
+    if (e.key === "Enter") {
+      addItem();
+    }
+  };
+
+  return (
+    <div className="d-flex flex-column justify-content-center align-items-center h-100">
+      <div className="col-12 col-sm-8 col-md-6 col-lg-4">
+        <div className="container d-flex flex-column justify-content-center w-100 min-vh-100">
+          <div className="card shadow-lg p-4">
+            <div className="d-flex flex-column align-items-center">
+              <div className="heading">
+                <h1>TO-DO-APP</h1>
+              </div>
+
+              <div className="d-flex w-100 my-3">
+                <input
+                  className="form-control"
+                  type="text"
+                  value={input}
+                  onChange={handleChange}
+                  onKeyDown={handleKeyDown}
+                  required
+                ></input>
+                <button
+                  onClick={() => addItem()}
+                  className="btn btn-success ms-2"
+                >
+                  <span>ADD</span>
+                </button>
+              </div>
+            </div>
+
+            <div className="todolist">
+              {items.map((item) => {
+                const { _id, name } = item;
+                return <TodoItem name={name} id={_id} setItems={setItems} />;
+              })}
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/TodoItem.js
@@ -0,0 +1,39 @@
+import React, { useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap-icons/font/bootstrap-icons.css";
+
+const API_BASE = "http://localhost:5000/todo";
+
+function TodoItem(props) {
+  const { name, id, setItems } = props;
+
+  const deleteTodo = async (id) => {
+    try {
+      //   console.log(id);
+      const response = await fetch(API_BASE + "/delete/" + id, {
+        method: "DELETE",
+      });
+      if (!response.ok) {
+        throw new Error("Faild to delete a task");
+      }
+      const data = await response.json();
+      setItems((items) => items.filter((item) => item._id !== data._id));
+    } catch (error) {
+      console.error("Error updating task status:", error);
+    }
+  };
+  return (
+    <div className="bg-primary   text-white rounded-3 my-2 p-2">
+      <div className="container d-flex justify-content-between">
+        <div className="text">{name}</div>
+        <div className="delete-todo" onClick={() => deleteTodo(id)}>
+          <span>
+            <i class="bi bi-trash3-fill"></i>
+          </span>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default TodoItem;
